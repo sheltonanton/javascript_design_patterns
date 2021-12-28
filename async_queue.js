f67296a397fe42ffc7005b9de3965f96f8d14f1d
sheltonanton <sheltonanton@gmail.com>
2021-12-29 04:28:57 +0530

    Async queue implemented

--- async_queue.js
+++ async_queue.js
@@ -7,3 +7,80 @@
  * 5. after termination, all pending promises should be resolved with EOD (End of Data) static value
  * 6. all the incoming events should be thrown as an Error, where the event pushing take place
  */
+
+const AsyncQueue = (() => {
+    const EOD = Symbol("EOD");
+    class AsyncQueue {
+        constructor() {
+            this.queue = [];
+            this.resolvers = [];
+            this.closed = false;
+        }
+
+        enqueue(data) {
+            if(this.closed) {
+                throw new Error("AsyncQueue is closed indefinitely");
+            }else if(this.resolvers.length > 0) {
+                const resolver = this.resolvers.shift();
+                resolver(data);
+            }else{
+                this.queue.push(data);
+            }
+        }
+
+        dequeue() {
+            if(this.queue.length > 0) {
+                return Promise.resolve(this.queue.shift());
+            }else if(this.closed) {
+                return Promise.resolve(EOD);
+            }else {
+                return new Promise((resolve) => {
+                    this.resolvers.push(resolve);
+                });
+            }
+        }
+
+        [Symbol.asyncIterator]() {
+            return this;
+        }
+
+        close() {
+            while(this.resolvers.length > 0) {
+                const resolver = this.resolvers.shift();
+                resolver(EOD);
+            }
+            this.closed = true;
+        }
+
+        isEOD(symbol) {
+            return this.EOD === symbol;
+        }
+
+        next() {
+            return this.dequeue().then(value => (this.isEOD(value))? { value: undefined, done: true } : { value: value, done: false });
+        }
+    }
+
+    return AsyncQueue;
+})();
+
+
+const queue = new AsyncQueue();
+for(let i=0; i < 10; i++) {
+    setTimeout(() => {
+        queue.enqueue(i);
+    }, Math.random() * 10000);
+}
+
+const execute = async () => {
+    let count = 0;
+    for await(const data of queue) {
+        console.log(data);
+        count += 1;
+        if(count == 10) {
+            break;
+        }
+    }
+}
+
+execute();
