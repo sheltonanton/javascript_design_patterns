4ec4a9c37c73ccb67c11d6210cc1db7e3acbfd4a
sheltonanton <sheltonanton@gmail.com>
2021-12-30 09:20:46 +0530

    Publisher/Subscriber pattern added

---
+++ pub_sub.js
@@ -0,0 +1,61 @@
+/**
+ * Requirements
+ * 1. a behavioural pattern, which is a great way that allows objects to communicate with one another, without depending on each other
+ * 2. it implements a module, which contains the subscrbed callbacks
+ * 3. subscribe, unsubscribe and publish function
+ */
+
+class PubSub {
+    topics = {}
+
+    publish(topic, data) {
+        if(!this.topics[topic]) {
+            return;
+        }
+        this.topics[topic].forEach(callback => callback(data));
+    }
+
+    subscribe(topic, callback) {
+        if(!this.topics[topic]) {
+            this.topics[topic] = [];
+        }
+
+        this.topics[topic].push(callback);
+        return {
+            unsubscribe: () => {
+                const index = this.topics[topic].indexOf(callback);
+                if(index !== -1) {
+                    this.topics[topic].splice(index, 1);
+                }
+            }
+        }
+    }
+}
+
+//testing
+
+const pubsub = new PubSub();
+
+const hello = pubsub.subscribe('hello', function(data) {
+    console.log('hello ' + data);
+});
+
+const world = pubsub.subscribe('world', function(data) {
+    console.log('world ' + data);
+});
+
+for(let i=0; i < 20; i++) {
+    if(i == 5) {
+        world.unsubscribe();
+    }
+    if(i == 10) {
+        pubsub.subscribe('hello', function(data) {
+            console.log('hello2 ' + data);
+        });
+    }
+    if(i % 2 == 0) {
+        pubsub.publish('hello', i);
+    }else{
+        pubsub.publish('world', i);
+    }
+}
